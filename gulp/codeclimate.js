d751d77c2ac047ff4e76e83d1cb742d413ed8d05
Andrew Nichols <tandrewnichols@gmail.com>
2016-01-19 09:31:26 -0500

    Add codeclimate coverage token

--- gulp/codeclimate.js
+++ gulp/codeclimate.js
@@ -5,7 +5,7 @@ gulp.task('codeclimate', function() {
   if (process.version.indexOf('v4') > -1) {
     gulp.src('coverage/lcov.info', { read: false })
       .pipe(codeclimate({
-        token: ''
+        token: '55df4cb5f81fb746410bac918d28ad9be945f8083310421f1cb280e9aae88a78'
       }));
   }
 });
